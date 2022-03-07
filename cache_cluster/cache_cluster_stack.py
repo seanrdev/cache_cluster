366b6e36de1190cfd201cdab5f0d4829b7d0df2c
sean <seanrdev@gmail.com>
2022-03-07 07:55:00 -0600

    Creation on Vpc, ASG, and Ec2 instance.

--- cache_cluster/cache_cluster_stack.py
+++ cache_cluster/cache_cluster_stack.py
@@ -2,18 +2,27 @@ from aws_cdk import (
     # Duration,
     Stack,
     # aws_sqs as sqs,
+    aws_ec2 as ec2,
+    aws_autoscaling as autoscaling,
 )
 from constructs import Construct
+
 
 class CacheClusterStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
-
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "CacheClusterQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        vpc = ec2.Vpc(self, "VPC")
+        asg = autoscaling.AutoScalingGroup(
+            self,
+            "ASG",
+            vpc=vpc,
+            instance_type=ec2.InstanceType.of(
+                ec2.InstanceClass.BURSTABLE2, ec2.InstanceSize.MICRO
+            ),
+            machine_image=ec2.AmazonLinuxImage(generation=ec2.AmazonLinuxGeneration.AMAZON_LINUX_2),
+            # user_data=some_defined_data_not_defined_yet
+            min_capacity=1,
+            max_capacity=2,
+        )
