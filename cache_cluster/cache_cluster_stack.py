314f15d081ace6ea76dd6a1b5e470c8b0761f515
sean <seanrdev@gmail.com>
2022-03-07 21:29:30 -0600

    Finished creation load balance. Error in Elasticache.
    Added User Data. Created Security Group.
    
    TODO: Subnet Group's

--- cache_cluster/cache_cluster_stack.py
+++ cache_cluster/cache_cluster_stack.py
@@ -4,6 +4,9 @@ from aws_cdk import (
     # aws_sqs as sqs,
     aws_ec2 as ec2,
     aws_autoscaling as autoscaling,
+    aws_elasticloadbalancingv2 as elb,
+    aws_elasticache as elcache,
+    aws_rds as rds,
 )
 from constructs import Construct
 
@@ -13,7 +16,29 @@ class CacheClusterStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
+        userdata = ec2.UserData.for_linux(shebang="#!/bin/bash")
+        userdata.add_commands(
+            "yum update -y",
+            "yum install -y httpd",
+            "systemctl start httpd",
+            "systemctl enable httpd",
+            "\"<h1>Hello World from $(hostname -f)</h1>\" >> / var / www / html / index.html",
+        )
         vpc = ec2.Vpc(self, "VPC")
+        sg_ec2 = ec2.SecurityGroup(
+            self,
+            id="ec2-sample-secgroup",
+            vpc=vpc,
+            security_group_name="ec2-sample-secgroup"
+        )
+        subnet_group = rds.SubnetGroup(
+            self,
+            'MySubnetGroup',
+            vpc=vpc,
+            description='My rds Sub',
+            subnet_group_name="sample-subnet-group"
+        )
+
         asg = autoscaling.AutoScalingGroup(
             self,
             "ASG",
@@ -23,6 +48,42 @@ class CacheClusterStack(Stack):
             ),
             machine_image=ec2.AmazonLinuxImage(generation=ec2.AmazonLinuxGeneration.AMAZON_LINUX_2),
             # user_data=some_defined_data_not_defined_yet
-            min_capacity=1,
-            max_capacity=2,
+            user_data=userdata,
+            security_group=sg_ec2,
+            min_capacity=2,
+            max_capacity=4,
+        )
+
+        lb = elb.ApplicationLoadBalancer(
+            self,
+            "lb",
+            vpc=vpc,
+            internet_facing=True,
+        )
+
+        listener = lb.add_listener(
+            "Listener",
+            port=80,
+            open=True,
+        )
+
+        listener.add_targets(
+            "ApplicationCluster",
+            port=80,
+            targets=[asg],
+        )
+
+        # cache.t4g.micro
+        memcache = elcache.CfnCacheCluster(
+            self,
+            "ElastiCacheGroup",
+            cache_node_type="cache.t4g.micro",
+            engine="memcached",
+            cache_subnet_group_name=subnet_group.subnet_group_name,
+            num_cache_nodes=4,
+            az_mode="cross-az",
+            cluster_name="SampleCluster",
+            engine_version="1.6.6",
+            port=43334,
+            vpc_security_group_ids=[sg_ec2.security_group_id],
         )
